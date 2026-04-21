fc5292a4aa048fb8a34bf36dec4cb04f054ff9f6
Prosper1107 <prosper.degue@epitech.eu>
2026-04-21 21:45:10 +0100

    Add Next.js configuration for image domains

---
+++ flash-wallet/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "mintlify.s3.us-west-1.amazonaws.com"],
+  },
+};
+
+module.exports = nextConfig;
