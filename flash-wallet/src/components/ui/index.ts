35bbeedafdff0241f90569bb06fdd0e8edf51ebf
Prosper1107 <prosper.degue@epitech.eu>
2026-04-21 22:01:38 +0100

    Add index file to export UI components for easier imports

---
+++ flash-wallet/src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Card, CardHeader, CardTitle } from "./Card";
+export { Badge, StatusBadge } from "./Badge";
+export { Spinner, PageLoader } from "./Spinner";
+export { EmptyState } from "./EmptyState";
